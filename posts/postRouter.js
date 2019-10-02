051df6ce1709abe9124b50e4037d90d9ec5c7e01
Daniel Wallen <daniel.wallen1987@gmail.com>
2019-10-02 18:10:50 -0400

    MVP met for userRouter (thought some of the post stuff was there by mistake until I looked at the endpoints closer, whoops!).

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,13 +1,19 @@
 const express = require('express');
 
+const Posts = require('./postDb');
+
 const router = express.Router();
 
 router.get('/', (req, res) => {
-
+    Posts.get(req.query)
+    .then(posts => res.status(200).json(posts))
+    .catch(err => res.status(500).json({ message: 'Failed to get posts' }))
 });
 
 router.get('/:id', (req, res) => {
-
+    Posts.getById(req.params.id)
+    .then(post => res.status(200).json(post))
+    .catch(err => res.status(500).json({ message: 'Failed to get post' }))
 });
 
 router.delete('/:id', (req, res) => {
